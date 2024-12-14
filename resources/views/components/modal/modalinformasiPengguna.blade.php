384c22b5036cab2a9da3919171d0bd0e89370443
bimaryan <bimaryan046@gmail.com>
2024-12-14 20:47:58 +0700

    ok

--- resources/views/components/modal/modalinformasiPengguna.blade.php
+++ resources/views/components/modal/modalinformasiPengguna.blade.php
@@ -40,7 +40,7 @@
                                             <span
                                                 class="bg-green-100 text-green-800 text-xs font-medium me-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">
                                                 {{ $loan->barang->kategori->kategori ?? 'Kategori Tidak Diketahui' }}
-                                            </span>
+                                            </span>verifikasi
                                         </p>
                                     </div>
                                 </div>
