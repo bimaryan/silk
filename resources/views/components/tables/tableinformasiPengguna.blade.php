7bb45375dff02fbff050839a028fa64f76e59133
bimaryan <bimaryan046@gmail.com>
2024-12-19 12:15:30 +0700

    `Refactored tableinformasiPengguna.blade.php to only display peminjamans with status 'Dikembalikan'`

--- resources/views/components/tables/tableinformasiPengguna.blade.php
+++ resources/views/components/tables/tableinformasiPengguna.blade.php
@@ -36,57 +36,56 @@
                     </td>
                 </tr>
             @else
-                {{-- @if ($peminjamans->first()->first()->status !== 'Dipinjamkan')
-
-                @endif --}}
-                @foreach ($peminjamans as $userId => $loans)
-                    <tr
-                        class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                        <td class="px-6 py-3">{{ $loop->iteration }}</td>
-                        <td class="px-6 py-3">
-                            @if ($loans->first()->mahasiswa_id)
-                                {{ $loans->first()->mahasiswa->nama }}
-                            @elseif ($loans->first()->dosen_id)
-                                {{ $loans->first()->dosen->nama }}
-                            @endif
-                        </td>
-                        <td class="px-6 py-3">
-                            {{ $loans->first()->nama_dosen ?? '-' }}
-                        </td>
-                        <td class="px-6 py-3">
-                            {{ $loans->first()->matkul->mata_kuliah ?? '-' }}
-                        </td>
-                        <td class="px-6 py-3">
-                            {{ $loans->first()->ruangan->nama_ruangan ?? '-' }}
-                        </td>
-                        <td class="px-6 py-3">
-                            {{ $loans->first()->tanggal_waktu_peminjaman ?? '-' }}
-                        </td>
-                        <td class="px-6 py-3">
-                            @php
-                                $status = $loans->first()->status;
-                            @endphp
-                            <span
-                                class="text-{{ $status === 'Menunggu Persetujuan' ? 'yellow' : ($status === 'Dipinjamkan' ? 'green' : 'red') }}-100 text-{{ $status === 'Menunggu Persetujuan' ? 'yellow' : ($status === 'Dipinjamkan' ? 'green' : 'red') }}-500 text-xs">
-                                {{ $status }}
-                            </span>
-                        </td>
-                        <td class="flex items-center gap-2 px-6 py-3 justify-center">
-                            <button type="button" data-modal-target="detail{{ $userId }}"
-                                data-modal-toggle="detail{{ $userId }}"
-                                class="flex items-center px-2 py-2 text-sm text-white bg-yellow-400 rounded">
-                                <i class="fa-solid fa-eye"></i>
-                            </button>
-                            {{-- <button type="button" data-modal-target="pengembalian{{ $userId }}"
+                @if ($peminjamans->first()->first()->status === 'Dikembalikan')
+                    @foreach ($peminjamans as $userId => $loans)
+                        <tr
+                            class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                            <td class="px-6 py-3">{{ $loop->iteration }}</td>
+                            <td class="px-6 py-3">
+                                @if ($loans->first()->mahasiswa_id)
+                                    {{ $loans->first()->mahasiswa->nama }}
+                                @elseif ($loans->first()->dosen_id)
+                                    {{ $loans->first()->dosen->nama }}
+                                @endif
+                            </td>
+                            <td class="px-6 py-3">
+                                {{ $loans->first()->nama_dosen ?? '-' }}
+                            </td>
+                            <td class="px-6 py-3">
+                                {{ $loans->first()->matkul->mata_kuliah ?? '-' }}
+                            </td>
+                            <td class="px-6 py-3">
+                                {{ $loans->first()->ruangan->nama_ruangan ?? '-' }}
+                            </td>
+                            <td class="px-6 py-3">
+                                {{ $loans->first()->tanggal_waktu_peminjaman ?? '-' }}
+                            </td>
+                            <td class="px-6 py-3">
+                                @php
+                                    $status = $loans->first()->status;
+                                @endphp
+                                <span
+                                    class="text-{{ $status === 'Menunggu Persetujuan' ? 'yellow' : ($status === 'Dipinjamkan' ? 'green' : 'red') }}-100 text-{{ $status === 'Menunggu Persetujuan' ? 'yellow' : ($status === 'Dipinjamkan' ? 'green' : 'red') }}-500 text-xs">
+                                    {{ $status }}
+                                </span>
+                            </td>
+                            <td class="flex items-center gap-2 px-6 py-3 justify-center">
+                                <button type="button" data-modal-target="detail{{ $userId }}"
+                                    data-modal-toggle="detail{{ $userId }}"
+                                    class="flex items-center px-2 py-2 text-sm text-white bg-yellow-400 rounded">
+                                    <i class="fa-solid fa-eye"></i>
+                                </button>
+                                {{-- <button type="button" data-modal-target="pengembalian{{ $userId }}"
                                 data-modal-toggle="pengembalian{{ $userId }}"
                                 class="flex items-center px-2 py-2 text-sm text-white bg-purple-500 rounded">
                                 <i class="fa-solid fa-boxes-packing"></i>
                             </button> --}}
-                        </td>
-                    </tr>
-                @endforeach
-                @include('components.modal.modalinformasiPengguna')
-                {{-- @include('components.modal.modalpengembalianPengguna') --}}
+                            </td>
+                        </tr>
+                    @endforeach
+                    @include('components.modal.modalinformasiPengguna')
+                    {{-- @include('components.modal.modalpengembalianPengguna') --}}
+                @endif
             @endif
         </tbody>
     </table>
